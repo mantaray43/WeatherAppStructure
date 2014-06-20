7ef098bb0ae7c530f40b5de9783897b8e20ef2df
cheryl ray <clr64@sbcglobal.net>
2014-06-19 19:21:11 -0500

    commit before modifying location manager

--- src/main/java/com/weatherornot/nightmare/Email_Activity.java
+++ src/main/java/com/weatherornot/nightmare/Email_Activity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,7 +16,7 @@ import android.widget.TextView;
 public class Email_activity extends Activity {
     static final String PREFERENCES = "temps";
 
-    String to  = "moreweatherpants@gmail.com";
+    String to = "moreweatherpants@gmail.com";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,8 +26,16 @@ public class Email_activity extends Activity {
         TextView x = (TextView) findViewById(R.id.linkview);
 
 
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle presses on the action bar items
@@ -35,25 +45,26 @@ public class Email_activity extends Activity {
                 SharedPreferences myPrefs = getSharedPreferences(PREFERENCES, 0);
                 SharedPreferences.Editor editor = myPrefs.edit();
                 myPrefs.edit().clear().commit();
-                Intent boo = new Intent(this,DisplaySettingsActivity.class);
+                Intent boo = new Intent(this, DisplaySettingsActivity.class);
                 startActivity(boo);
 
                 return true;
 
             case R.id.email:
-                Intent email = new Intent(this,Email_activity.class);
+                Intent email = new Intent(this, Email_activity.class);
                 startActivity(email);
                 return true;
 
             case R.id.rate:
-                Intent rate = new Intent(this,RateMyAPPActivity.class);
+                Intent rate = new Intent(this, RateMyAPPActivity.class);
                 startActivity(rate);
-
 
 
             default:
                 return super.onOptionsItemSelected(item);
         }
+
+
     }
 
 }
